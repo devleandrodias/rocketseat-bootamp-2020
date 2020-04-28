92eb718455b7b575ab0f00c2e73eea3280422a37
Leandro <leandrodbdias@gmail.com>
2020-04-28 18:07:33 -0300

    Finalização de uma aplicação funcional com nodejs

--- nivel-01/src/index.js
+++ nivel-01/src/index.js
@@ -1,6 +1,9 @@
 const express = require("express");
+const { uuid } = require("uuidv4");
 
 const app = express();
+
+app.use(express.json());
 
 /**
  * Métodos HTTP:
@@ -11,26 +14,63 @@ const app = express();
  * DELETE: Deletar uma informação no backend
  */
 
-app.get("/projects", (_, res) => {
-  return res.json([
-    {
-      id: 1,
-      name: "Project Alpha",
-      description: "Description project alpha",
-    },
-    {
-      id: 2,
-      name: "Project Bravo",
-      description: "Description project bravo",
-    },
-  ]);
+/**
+ * Tipos de parâmetros:
+ *
+ * Query params: Filtros e paginação
+ * Route params:
+ * Request body:
+ */
+
+const projects = [];
+
+app.post("/projects", (req, res) => {
+  const { owner } = req.query;
+  const body = req.body;
+
+  projects.push({ id: uuid(), owner, ...body });
+
+  return res.send(body);
 });
 
-app.post("/projects", (req, _) => console.log(req.body.name));
+app.get("/projects", (_, res) => {
+  return res.json(projects);
+});
 
-app.put("/projects/:id", (req, _) => console.log(req.params.id));
+app.get("/projects/:id", (req, res) => {
+  const { id } = req.params;
+  return res.json(projects.find((project) => project.id === id));
+});
 
-app.delete("/projects/:id", (req, res) => console.log(req.params.id));
+app.put("/projects/:id", (req, res) => {
+  const { id } = req.params;
+  const { owner } = req.query;
+  const body = req.body;
+
+  const projectIndex = projects.find((project) => project.id === id);
+
+  if (projectIndex < 0)
+    return res.status(400).json({ error: "Project nor found" });
+
+  const project = { id, owner, ...body };
+
+  projects[projectIndex] = project;
+
+  return res.json(project);
+});
+
+app.delete("/projects/:id", (req, res) => {
+  const { id } = req.params;
+
+  const projectIndex = projects.find((project) => project.id === id);
+
+  if (projectIndex < 0)
+    return res.status(400).json({ error: "Project nor found" });
+
+  projects.slice(projectIndex, 1);
+
+  return res.send().status(204);
+});
 
 app.listen(3333, () => {
   console.log("🎯Iniciando o backend...");
