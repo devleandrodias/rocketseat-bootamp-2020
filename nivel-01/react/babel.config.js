6f81f1f0457882db6671c50487435683ad64199a
Leandro <leandrodbdias@gmail.com>
2020-04-29 00:55:36 -0300

    Criação de um projeto base de react e configuração do babel

---
+++ nivel-01/react/babel.config.js
@@ -0,0 +1,11 @@
+/**
+ * Presets: São conjuntos de configurações criadas por terceiros
+ *
+ * @babel/preset-env, convert javascript para um javascript mais antigo e
+ * converter apenas funcionalida que os navegadores ainda não entendem (env (Ambiente))
+ *
+ * @babel/preset-react adicona as funcionalidade do react pra transpilação
+ */
+module.exports = {
+  presets: ["@babel/preset-env", "@babel/preset-react"],
+};
