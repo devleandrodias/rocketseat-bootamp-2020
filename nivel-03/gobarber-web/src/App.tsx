d9ffc4b51ea6535d0d1a325501b51cc58626bfd3
devleandrodias <leandrodbdias@gmail.com>
2020-05-19 21:03:45 -0300

    estilizacao global da aplicacao gobarber

--- nivel-03/gobarber-web/src/App.tsx
+++ nivel-03/gobarber-web/src/App.tsx
@@ -1,9 +1,14 @@
 import React from 'react';
 
+import GlobalStyle from './styles/global';
+
 const App: React.FC = () => (
-  <div className="App">
-    <h1>Hello World</h1>
-  </div>
+  <>
+    <GlobalStyle />
+    <div className="App">
+      <h1>Hello World</h1>
+    </div>
+  </>
 );
 
 export default App;
