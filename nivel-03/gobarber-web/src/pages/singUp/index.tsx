92c74850e1908d8b8d810af3f0829312bfd05909
devleandrodias <leandrodbdias@gmail.com>
2020-05-21 16:03:08 -0300

    validando erros no formulário

--- nivel-03/gobarber-web/src/pages/singUp/index.tsx
+++ nivel-03/gobarber-web/src/pages/singUp/index.tsx
@@ -1,5 +1,6 @@
-import React, { useCallback } from 'react';
+import React, { useCallback, useRef } from 'react';
 import { FiArrowLeft, FiMail, FiLock, FiUser } from 'react-icons/fi';
+import { FormHandles } from '@unform/core';
 import { Form } from '@unform/web';
 import * as Yup from 'yup';
 
@@ -9,10 +10,15 @@ import Input from '../../components/input';
 import Button from '../../components/button';
 
 import logoImg from '../../assets/logo.svg';
+import getValidationErros from '../../utils/getValidationsErros';
 
 const SingUp: React.FC = () => {
+  const formRef = useRef<FormHandles>(null);
+
   const handleSubmit = useCallback(async (data: object) => {
     try {
+      formRef.current?.setErrors({});
+
       const schema = Yup.object().shape({
         name: Yup.string().required('Nome é obrigatório'),
         password: Yup.string().min(6, 'Senha deve ter no mínimo 6 caracteres'),
@@ -23,7 +29,9 @@ const SingUp: React.FC = () => {
 
       await schema.validate(data, { abortEarly: false });
     } catch (error) {
-      console.log(error);
+      const errors = getValidationErros(error);
+
+      formRef.current?.setErrors(errors);
     }
   }, []);
 
@@ -32,7 +40,7 @@ const SingUp: React.FC = () => {
       <Background />
       <Content>
         <img src={logoImg} alt="gobarber-logo" />
-        <Form initialData={{}} onSubmit={handleSubmit}>
+        <Form ref={formRef} onSubmit={handleSubmit}>
           <h1>Faça seu cadastro</h1>
 
           <Input name="name" icon={FiUser} type="text" placeholder="Nome" />
