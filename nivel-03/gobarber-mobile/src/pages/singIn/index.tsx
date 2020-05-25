9db8d664afe72f42a2b61b64851bc9f4f61a3bcb
devleandrodias <leandrodbdias@gmail.com>
2020-05-25 14:16:29 -0300

    configurando rotas dentro do react native

---
+++ nivel-03/gobarber-mobile/src/pages/singIn/index.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+import { Container } from './styles';
+
+const singIn: React.FC = () => {
+  return <Container />;
+};
+
+export default singIn;
---
+++ nivel-03/gobarber-mobile/src/pages/singUp/index.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+import { Container } from './styles';
+
+const singUp: React.FC = () => {
+  return <Container />;
+};
+
+export default singUp;
