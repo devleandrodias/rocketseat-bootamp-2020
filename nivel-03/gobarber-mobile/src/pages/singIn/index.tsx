ade0c1833f6dbcb56d7a063dc64d73b5b8aef342
devleandrodias <leandrodbdias@gmail.com>
2020-05-26 09:15:23 -0300

    estilizando pagina de SingIn e criando componentes de input e button do app

--- nivel-03/gobarber-mobile/src/pages/singIn/index.tsx
+++ nivel-03/gobarber-mobile/src/pages/singIn/index.tsx
@@ -3,6 +3,9 @@ import { Image } from 'react-native';
 
 import { Container, Title } from './styles';
 
+import Input from '../../components/Input';
+import Button from '../../components/Button';
+
 import logoImg from '../../assets/logo.png';
 
 const singIn: React.FC = () => {
@@ -10,6 +13,9 @@ const singIn: React.FC = () => {
     <Container>
       <Image source={logoImg} />
       <Title>Faça seu logon</Title>
+      <Input name="email" icon="mail" placeholder="E-Mail" />
+      <Input name="password" icon="lock" placeholder="Senha" />
+      <Button onPress={() => {}}>Entrar</Button>
     </Container>
   );
 };
