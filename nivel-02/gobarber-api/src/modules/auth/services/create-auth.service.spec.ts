508fd37f3cced123deaebcd1672e26499e943239
devleandrodias <leandrodbdias@gmail.com>
2020-05-31 12:24:57 -0300

    adicionando mais testes a autentificacao

--- nivel-02/gobarber-api/src/modules/auth/services/create-auth.service.spec.ts
+++ nivel-02/gobarber-api/src/modules/auth/services/create-auth.service.spec.ts
@@ -5,6 +5,7 @@ import { FakeHashProvider } from '@modules/users/providers/hash-providers/fakes/
 
 import CreateUserService from '@modules/users/services/create-user.service';
 import AuthenticateService from './create-auth.service';
+import AppError from '@shareds/errors/app.error';
 
 describe('AuthenticateUser', () => {
   it('deve criar a autentificação para o usuário', async () => {
@@ -34,5 +35,22 @@ describe('AuthenticateUser', () => {
 
     expect(auth).toHaveProperty('token');
     expect(auth.user).toEqual(user);
+  });
+
+  it('não deve permitir criar uma autentificação para um usuário que não existe', async () => {
+    const fakeUserRepository = new FakeUserRepository();
+    const fakeHashProvider = new FakeHashProvider();
+
+    const createAuthenticateService = new AuthenticateService(
+      fakeUserRepository,
+      fakeHashProvider
+    );
+
+    expect(
+      createAuthenticateService.execute({
+        email: 'johndoe@example.com',
+        password: '123456',
+      })
+    ).rejects.toBeInstanceOf(AppError);
   });
 });
